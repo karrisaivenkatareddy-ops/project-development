4c30b10e6d757a9a50bcce0baf5b36ecd1d4899e
saivenkatareddy168 <saivenkatareddyk@gmail.com>
2026-07-29 16:25:41 +0530

    added authentication

--- auth.py
+++ auth.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter(
-prefix="/auth",
-tags=["Authentication"]
+    prefix="/auth",
+    tags=["Authentication"]
 )
 
 users = {}
@@ -22,33 +22,29 @@ def register(username: str, password: str):
         "username": username
     }
 
+
 @router.post("/login")
 def login(username: str, password: str):
+    if username not in users:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
 
-```
-if username not in users:
-    raise HTTPException(
-        status_code=404,
-        detail="User not found"
-    )
+    if users[username] != password:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect password"
+        )
 
-if users[username] != password:
-    raise HTTPException(
-        status_code=401,
-        detail="Incorrect password"
-    )
+    return {
+        "message": "Login successful",
+        "username": username
+    }
 
-return {
-    "message": "Login successful",
-    "username": username
-}
-```
 
 @router.get("/users")
 def show_users():
-
-```
-return {
-    "registered_users": list(users.keys())
-}
-```
+    return {
+        "registered_users": list(users.keys())
+    }

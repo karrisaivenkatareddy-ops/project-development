5f139bf63a992603d52e56d5391f5c76be902562
Harshini vemula <harshini.vemula28@gmail.com>
2026-08-08 12:42:04 +0530

    authController.js file added

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+exports.register = (req,res)=>{
+
+    res.json({
+
+        success:true,
+
+        message:"User Registered Successfully"
+
+    });
+
+}
+
+exports.login=(req,res)=>{
+
+    res.json({
+
+        success:true,
+
+        message:"Login Successful"
+
+    });
+
+}
